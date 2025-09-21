06d1542c544e499de055cb20c123b9b8785b1297
Rakesh K <codersgyan@gmail.com>
2025-09-21 21:51:27 +0300

    add tailwind csss

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import KanbanBoard from './components/kanban';
+
+function App() {
+    return (
+        <>
+            <KanbanBoard />
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/kanban.tsx
@@ -0,0 +1,5 @@
+const KanbanBoard = () => {
+    return <h1 className="text-red-500">Kanban board</h1>;
+};
+
+export default KanbanBoard;
