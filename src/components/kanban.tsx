b83d08135904b25a3931ec07e76da8fc90d03254
Manish Dahiya <manishdahiya0944@gmail.com>
2025-09-22 00:51:50 +0530

    add tasks filter acc to status

--- src/components/kanban.tsx
+++ src/components/kanban.tsx
@@ -1,4 +1,5 @@
 import tasks from "../data/tasks.json";
+import type { Task } from "../types/task";
 
 const columns = [
   {
@@ -6,18 +7,21 @@ const columns = [
     color: "bg-slate-700",
     header: "text-blue-300",
     border: "border-blue-500",
+    status: "TODO",
   },
   {
     title: "In Progress",
     color: "bg-slate-700",
     header: "text-yellow-300",
     border: "border-yellow-500",
+    status: "IN_PROGRESS",
   },
   {
     title: "Done",
     color: "bg-slate-900",
     header: "text-green-300",
     border: "border-green-500",
+    status: "DONE",
   },
 ];
 
@@ -37,17 +41,30 @@ const KanbanBoard = () => {
               {col.title}
             </h2>
             <div className="flex flex-col gap-4">
-              {tasks.map((task) => (
-                <div
-                  key={task.id}
-                  className={`bg-slate-800 rounded-lg shadow p-4 border-l-4 ${col.border} hover:shadow-md transition`}
-                >
-                  <div className="font-medium text-slate-100">{task.title}</div>
-                  <div className="text-slate-300 text-sm">
-                    {task.description}
+              {(tasks as Task[])
+                .filter((task: Task) => task.status === col.status)
+                .map((task: Task) => (
+                  <div
+                    key={task.id}
+                    className={`bg-slate-800 rounded-lg shadow p-4 border-l-4 ${col.border} hover:shadow-md transition`}
+                  >
+                    <div className="font-medium text-slate-100">
+                      {task.title}
+                    </div>
+                    <div className="text-slate-300 text-sm">
+                      {task.description}
+                    </div>
+                    <div className="text-slate-400 text-xs mt-2 flex justify-between">
+                      <span>User ID: {task.asignedTo.name}</span>
+                      <span>
+                        Created:{" "}
+                        {task.createdAt
+                          ? new Date(task.createdAt).toLocaleDateString()
+                          : "N/A"}
+                      </span>
+                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
             </div>
           </div>
         ))}
